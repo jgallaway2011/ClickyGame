5200bac6e538dd7102bdfa770ef6cb3a3811c276
jgallaway2011 <jgallaway2011@gmail.com>
2018-10-18 00:18:43 -0500

    Added functionality for cards to shuffle when clicked

--- src/App.js
+++ src/App.js
@@ -6,10 +6,27 @@ import Jumbotron from "./components/Jumbotron";
 import ImageCard from "./components/ImageCard"
 import "./App.css";
 
+function shuffleImages(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     images
+  };
+
+  handleShuffle = () => {
+    let shuffledImages = shuffleImages(images);
+    this.setState({ images: shuffledImages });
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
@@ -21,9 +38,10 @@ class App extends Component {
         <div className="row align-items-center">
         {this.state.images.map(image => (
           <ImageCard
-            id={image.id}
             key={image.id}
+            handleShuffle={this.handleShuffle}
             name={image.name}
+            id={image.id}
             image={image.image}
           />
         ))}
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -10,7 +10,7 @@ const styles = {
 const FriendCard = props => (
   <div className="col-sm-6 col-md-4 col-lg-3" key={props.id}>
     <div className="card ml-auto mr-auto" style={styles.card}>
-      <img className="card-img-top" src={props.image} alt={props.name} onClick={() => props.shuffleImages()}/>
+      <img className="card-img-top" src={props.image} alt={props.name} onClick={() => props.handleShuffle()}/>
     </div>
   </div>
 );
