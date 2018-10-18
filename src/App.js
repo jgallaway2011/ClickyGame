89f3528ff53c3b9cd216ba5d51b5ef58c371dca2
jgallaway2011 <jgallaway2011@gmail.com>
2018-10-18 00:04:37 -0500

    Added pictures for app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
-import friends from "./friends.json";
+import images from "./images.json";
+import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
+import ImageCard from "./components/ImageCard"
 import "./App.css";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
-  };
-
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    images
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="container no-gutters">
+      <Wrapper>
         <Navbar />
         <Jumbotron />
-      </div>
+        <div className="row align-items-center">
+        {this.state.images.map(image => (
+          <ImageCard
+            id={image.id}
+            key={image.id}
+            name={image.name}
+            image={image.image}
+          />
+        ))}
+        </div>
+      </Wrapper>
     );
   }
 }
---
+++ src/components/ImageCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const styles = {
+  card: {
+    width: "200px",
+    height: "200px"
+  },
+};
+
+const FriendCard = props => (
+  <div className="col-sm-6 col-md-4 col-lg-3" key={props.id}>
+    <div className="card ml-auto mr-auto" style={styles.card}>
+      <img className="card-img-top" src={props.image} alt={props.name} onClick={() => props.shuffleImages()}/>
+    </div>
+  </div>
+);
+
+export default FriendCard;
