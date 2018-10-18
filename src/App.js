7f8df36b3494bfbc0a1f79bb86bb24adbc19e99e
jgallaway2011 <jgallaway2011@gmail.com>
2018-10-18 14:52:34 -0500

    Updated README.md

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
     this.setState({ images: shuffledImages });
   };
 
+  // Function in increment score when correct selection made
   handleIncrement = () => {
     const updatedScore = this.state.currentScore + 1;
     this.setState({
